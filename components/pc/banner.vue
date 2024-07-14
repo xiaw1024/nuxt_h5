f09d63cb89b693c82d7fdfeb98a1908bf4a444c5
liukai <1587680247@qq.com>
2024-07-14 23:46:39 +0800

    上拉加载

--- components/pc/banner.vue
+++ components/pc/banner.vue
@@ -1,22 +1,29 @@
 <template>
-<div class="banner">
+  <div class="banner">
     <div class="header_bg">
-        <img class="bg" src="~/assets/img/banner.png" alt="">
-        <div class="search">
-            <div class="search_box">
-                <input type="text" placeholder="search">
-                <img class="search_icon" src="~/assets/img/search.png" alt="">
-            </div>
+      <img class="bg" src="~/assets/img/banner.png" alt="" />
+      <div class="search">
+        <div class="search_box">
+          <input type="text" placeholder="search" />
+          <img class="search_icon" src="~/assets/img/search.png" alt="" />
         </div>
+      </div>
     </div>
     <img class="cover" src="~/assets/img/cover.png" alt="" />
 
     <div class="container">
+      <van-list
+        v-model:loading="loading"
+        :finished="finished"
+        finished-text="没有更多了"
+        @load="onLoad"
+      >
         <template v-for="item in List" :key="item.id">
-        <PcCard :data="item"></PcCard>
+          <PcCard :data="item"></PcCard>
         </template>
+      </van-list>
     </div>
-</div>
+  </div>
 </template>
 
 <script setup >
@@ -51,11 +58,13 @@
 //     }
 // ]
 // 查询指定栏目下的稿件列表：https://eicc.hndt.com/mobile/cms/articles?channelId=1810136737417199617&pageNo=1&pageSize=10 查询稿件详情：https://eicc.hndt.com/mobile/cms/articlewithrelated?articleId=1810137459849887745
-let List=ref([])
-let pageNo=1,pageSize=10
-let testNaveUrl='https://pubmod.hntv.tv/mobile/cms/channelsbyparentchanneid?channelId=1760954163992039426'
-let testListUrl='https://pubmod.hntv.tv/mobile/cms/articles?channelId=1760504763108921345&pageNo=1&pageSize=20'
-let prodUrl=`https://eicc.hndt.com/mobile/cms/articles?channelId=1810136737417199617&pageNo=${pageNo}&pageSize=${pageSize}`
+let List = ref([]);
+let pageSize = 3;
+let pageNo = 1;
+let testNaveUrl =
+  "https://pubmod.hntv.tv/mobile/cms/channelsbyparentchanneid?channelId=1760954163992039426";
+let testListUrl = `https://pubmod.hntv.tv/mobile/cms/articles?channelId=1760504763108921345&pageNo=${pageNo.value}&pageSize=${pageSize}`;
+let prodUrl = `https://eicc.hndt.com/mobile/cms/articles?channelId=1810136737417199617&pageNo=${pageNo.pageNo}&pageSize=${pageSize}`;
 // const getList = async()=>{
 //     const data = await useFetch(prodUrl)
 //     console.log(data,'data');
@@ -63,89 +72,114 @@ let prodUrl=`https://eicc.hndt.com/mobile/cms/articles?channelId=181013673741719
 //     console.log(code,msg,content,'sda')
 //     if(code != 0) return
 //     List.value=content
-    
+
 // }
 // getList()
+const loading = ref(false);
+const finished = ref(false);
 
-const getTestList = async()=>{
-    const data = await useFetch(testListUrl)
-    console.log(data,'data');
-    let {data:{_rawValue:{code,msg,result:{content}}}}=data
-    console.log(code,msg,content,'sda')
-    if(code != 0) return
-    List.value=content
-    
-}
-getTestList()
+const getTestList = async () => {
+  testListUrl = `https://pubmod.hntv.tv/mobile/cms/articles?channelId=1760504763108921345&pageNo=${pageNo}&pageSize=${pageSize}`;
+  const data = await useFetch(testListUrl);
 
-// console.log(List,'List');
+  let {
+    data: {
+      _rawValue: {
+        code,
+        msg,
+        result: { content },
+      },
+    },
+  } = data;
+  // console.log(code,msg,content,'sda')
+  if (code != 0) return;
+  if (content.length > 0) {
+    // 加载状态结束
+    loading.value = false;
+    if (List.value.length > 0) {
+      List.value = List.value.concat(content);
+    } else {
+      List.value = content;
+    }
+  } else {
+    finished.value = true;
+  }
+};
+getTestList();
+const onLoad = () => {
+  // 异步更新数据
+  // setTimeout 仅做示例，真实场景中一般为 ajax 请求
+  if (process.client) {
+    pageNo++;
+    getTestList();
+  }
+};
 </script>
-<style lang="scss" scoped> 
-.banner{
-    width: 100%;
-    min-width: 1200px;
-.header_bg{
+<style lang="scss" scoped>
+.banner {
+  width: 100%;
+  min-width: 1200px;
+  .header_bg {
     width: 100%;
     position: relative;
-    .bg{
-        width: 100%;
-        display: block;
-}
-    .search{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        position: absolute;
-        top: 0;
-        left: 0;
-        height: 100%;
-        bottom: 0;
-        right: 0;
-        .search_box{
-            width: 784px;
-            height: 80px;
-            background-image: url('~/assets/img/search_bg.png');
-            background-repeat: no-repeat;
-            background-size: contain;
-            display: flex;
-            justify-content: space-around;
-            align-items: center;
-            input{
-                width: 600px;
-                height: 53px;
-                background-color: rgba(255,255,255,0);
-                border: none;
-                outline: none;
-                font-size: 24px;
-                line-height: 53;
-                font-family: HelveticaNeue, HelveticaNeue;
-                font-weight: 500;
-                color: #fff;
-                &::placeholder{
-                    color: #fff;
-                }
-            }
-           
-            .search_icon{
-                width: 32px;
-                cursor: pointer;
-            }
-        }
+    .bg {
+      width: 100%;
+      display: block;
     }
-}
-.cover{
+    .search {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: absolute;
+      top: 0;
+      left: 0;
+      height: 100%;
+      bottom: 0;
+      right: 0;
+      .search_box {
+        width: 784px;
+        height: 80px;
+        background-image: url("~/assets/img/search_bg.png");
+        background-repeat: no-repeat;
+        background-size: contain;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        input {
+          width: 600px;
+          height: 53px;
+          background-color: rgba(255, 255, 255, 0);
+          border: none;
+          outline: none;
+          font-size: 24px;
+          line-height: 53;
+          font-family: HelveticaNeue, HelveticaNeue;
+          font-weight: 500;
+          color: #fff;
+          &::placeholder {
+            color: #fff;
+          }
+        }
+
+        .search_icon {
+          width: 32px;
+          cursor: pointer;
+        }
+      }
+    }
+  }
+  .cover {
     width: 100%;
     height: 137px;
     position: fixed;
     top: 0;
     left: 0;
+  }
 }
-
-}
-.container{
-    max-width: 1200px;
-    background-color: #fff;
-    margin: 0 auto;
-    padding: 30px 0 160px 0;
+.container {
+  max-width: 1200px;
+  background-color: #fff;
+  margin: 0 auto;
+  padding: 30px 0 160px 0;
 }
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,66 +1,74 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    // 页面路由
-    pages: true,
-    css: [
-        '@/assets/styles/pc/index.scss',
-      ],
-    // vite: {
-    //     css: {
-    //       preprocessorOptions: {
-    //         scss: {
-    //           additionalData: '@use "@/assets/styles/pc/index.scss" as *;', // 注意文件路径要配成自己的
-    //         },
-    //       },
-    //     }
-    //   },
-    app: {
-        head: {
-          viewport: 'width=device-width,initial-scale=1',
-          link: [
-            { rel: 'icon', href: './favicon.ico', sizes: 'any' }
-          ],
-          meta: [
-            {charset: 'utf-8'},
-            // { "http-equiv": "X-UA-Compatible", "content": "IE=edge" },
-            {"http-equiv": "pragma", "content": "no-cache"},
-            {"http-equiv": "Cache-Control", "content": "no-cache, must-revalidate"},
-            {"http-equiv": "expires", "content": "0"},
-            { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, user-scalable=0, viewport-fit=cover' },
-            { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent'},
-          ],
+  devtools: {
+    enabled: true // or false to disable
+  },
+
+  // 页面路由
+  pages: true,
+
+  css: [
+      '@/assets/styles/pc/index.scss',
+    ],
+
+  // vite: {
+  //     css: {
+  //       preprocessorOptions: {
+  //         scss: {
+  //           additionalData: '@use "@/assets/styles/pc/index.scss" as *;', // 注意文件路径要配成自己的
+  //         },
+  //       },
+  //     }
+  //   },
+  app: {
+      head: {
+        viewport: 'width=device-width,initial-scale=1',
+        link: [
+          { rel: 'icon', href: './favicon.ico', sizes: 'any' }
+        ],
+        meta: [
+          {charset: 'utf-8'},
+          // { "http-equiv": "X-UA-Compatible", "content": "IE=edge" },
+          {"http-equiv": "pragma", "content": "no-cache"},
+          {"http-equiv": "Cache-Control", "content": "no-cache, must-revalidate"},
+          {"http-equiv": "expires", "content": "0"},
+          { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, user-scalable=0, viewport-fit=cover' },
+          { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent'},
+        ],
+      },
+    },
+
+  // 自定义配置项
+  runtimeConfig: {
+    // 只在服务端可以访问的配置项
+    apiSecret: 'api-secret',
+    // 可以暴露给客户端使用的配置项
+    public: {},
+  },
+
+  postcss: {
+      plugins: {
+        'postcss-px-to-viewport-8-plugin': {
+          unitToConvert: 'px', // 要转化的单位
+          viewportWidth: 375, // UI设计稿的宽度
+          // viewportHeight: 667, // UI
+          unitPrecision: 6, // 转换后的精度，即小数点位数
+          // propList: 当有些属性的单位我们不希望转换的时候，可以添加在数组后面，并在前面加上!号，如propList: ["*","!letter-spacing"],这表示：所有css属性的属性的单位都进行转化，除了letter-spacing的
+          propList: ['*'], // 指定转换的css属性的单位，*代表全部css属性的单位都进行转换
+          // viewportUnit: 'vw', // 指定需要转换成的视窗单位，默认vw
+          // fontViewportUnit: 'vw', // 指定字体需要转换成的视窗单位，默认vw
+          exclude: [/node_modules/i,/components\/pc/i,/default/i],
+          // 转换的黑名单，在黑名单里面的我们可以写入字符串，只要类名包含有这个字符串，就不会被匹配。比如selectorBlackList: ['wrap'],它表示形如wrap,my-wrap,wrapper这样的类名的单位，都不会被转换
+          selectorBlackList: ['ignore', 'van'], // 指定不转换为视窗单位的类名，
+          minPixelValue: 1, // 默认值1，小于或等于1px则不进行转换
+          mediaQuery: true, // 是否在媒体查询的css代码中也进行转换，默认false
+          replace: true, // 是否转换后直接更换属性值
+          // exclude: [/node_modules/],zz // 设置忽略文件，用正则做目录名匹配
+          // include: [/node_modules/],
+          landscape: false, // 是否处理横屏情况
         },
       },
+    },
 
-
-      // 自定义配置项
-      runtimeConfig: {
-        // 只在服务端可以访问的配置项
-        apiSecret: 'api-secret',
-        // 可以暴露给客户端使用的配置项
-        public: {},
-      },
-    postcss: {
-        plugins: {
-          'postcss-px-to-viewport-8-plugin': {
-            unitToConvert: 'px', // 要转化的单位
-            viewportWidth: 375, // UI设计稿的宽度
-            // viewportHeight: 667, // UI
-            unitPrecision: 6, // 转换后的精度，即小数点位数
-            // propList: 当有些属性的单位我们不希望转换的时候，可以添加在数组后面，并在前面加上!号，如propList: ["*","!letter-spacing"],这表示：所有css属性的属性的单位都进行转化，除了letter-spacing的
-            propList: ['*'], // 指定转换的css属性的单位，*代表全部css属性的单位都进行转换
-            // viewportUnit: 'vw', // 指定需要转换成的视窗单位，默认vw
-            // fontViewportUnit: 'vw', // 指定字体需要转换成的视窗单位，默认vw
-            exclude: [/node_modules/i,/components\/pc/i,/default/i],
-            // 转换的黑名单，在黑名单里面的我们可以写入字符串，只要类名包含有这个字符串，就不会被匹配。比如selectorBlackList: ['wrap'],它表示形如wrap,my-wrap,wrapper这样的类名的单位，都不会被转换
-            selectorBlackList: ['ignore', 'van'], // 指定不转换为视窗单位的类名，
-            minPixelValue: 1, // 默认值1，小于或等于1px则不进行转换
-            mediaQuery: true, // 是否在媒体查询的css代码中也进行转换，默认false
-            replace: true, // 是否转换后直接更换属性值
-            // exclude: [/node_modules/],zz // 设置忽略文件，用正则做目录名匹配
-            // include: [/node_modules/],
-            landscape: false, // 是否处理横屏情况
-          },
-        },
-      },
+  modules: ['@vant/nuxt'],
 })
